14251f9ae01366495c9f73b5a90cdb043f8fc04b
Putri Yanasita <putriiyt37@gmail.com>
2024-12-28 12:55:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -122,24 +122,6 @@ app.get('/instagram', async (req, res) => {
   }
 });
 
-app.get('/luminai', async (req, res) => {
-    try {
-      const { text } = req.query;
-      if (!text) {
-        return res.status(400).json({ error: 'Parameter "text" Tidak Ditemukan, Tolong Masukkan Perintah' });
-      }
-      const response = await fetchContent(text);
-      res.status(200).json({
-        status: 200,
-        creator: author,
-        data: response
-      });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-};
-
 app.get('/capcut', async (req, res) => {
   const url = req.query.url;
   if (!url) return res.json(msg.paramurl);
@@ -199,6 +181,7 @@ app.get('/blackboxai', async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
 app.get('/aio', async (req, res) => {
   const url = req.query.url;
   if (!url) return res.json(msg.paramurl);
