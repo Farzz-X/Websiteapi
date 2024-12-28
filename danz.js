3d9d94f2df2ab134e1bf006fa733456ee232b121
Putri Yanasita <putriiyt37@gmail.com>
2024-12-28 14:55:39 +0700

    Update danz.js

--- danz.js
+++ danz.js
@@ -60,9 +60,9 @@ async function listmember(query) {
   }
 }
 
-async function luminai(url) {
+async function luminai(content) {
   try {
-    const response = await axios.post('https://luminai.my.id/chat', { url });
+    const response = await axios.post('https://luminai.my.id/', { content });
     return response.data;
   } catch (error) {
     console.error("Error fetching content from LuminAI:", error);
