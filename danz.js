072c473b21687415786ad3d9b3d4121f0cc3b1d5
Putri Yanasita <putriiyt37@gmail.com>
2024-12-28 13:44:03 +0700

    Update danz.js

--- danz.js
+++ danz.js
@@ -27,7 +27,36 @@ async function tiktok(url) {
     }
   });
 }
- 
+
+async function liputan6() {
+    try {
+      const AvoskyBaik = await axios.get('https://www.liputan6.com/');
+      const $ = cheerio.load(AvoskyBaik.data);
+
+      const latestNews = $('.articles--iridescent-list').eq(2).find('article');
+      const results = [];
+
+      latestNews.each(function () {
+        try {
+          const title = $(this).find('figure a').attr('title');
+          const link = $(this).find('figure a').attr('href');
+          const image = $(this).find('figure a picture img').attr('data-src');
+          const tag = $(this).find('aside header a').text();
+
+          results.push({ title, link, tag, image, source: 'liputan6' });
+        } catch (e) {
+          console.error('Error scraping article:', e);
+        }
+      });
+
+      return results;
+    } catch (error) {
+      console.error('Error fetching:', error);
+      return [];
+    }
+  }
+
+                          
 async function igdl(url) {
             let res = await axios("https://indown.io/");
             let _$ = cheerio.load(res.data);
