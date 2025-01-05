597dbe4e0f20595f3925176cf4ff278452e10597
FarisDev <akunkoshonganz@gmail.com>
2025-01-05 14:28:30 +0700

    Update danz.js

--- danz.js
+++ danz.js
@@ -27,6 +27,34 @@ async function tiktok(url) {
     }
   });
 }
+
+
+async function tiktokdl(query) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const encodedParams = new URLSearchParams();
+      encodedParams.set("url", query);
+      encodedParams.set("hd", "1");
+
+      const response = await axios({
+        method: "POST",
+        url: "https://tikwm.com/api/",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Cookie: "current_language=en",
+          "User-Agent":
+            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36",
+        },
+        data: encodedParams,
+      });
+      const videos = response.data;
+      resolve(videos);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
 async function pinterest(query) {
 return new Promise(async(resolve,reject) => {
 axios.get('https://id.pinterest.com/search/pins/?autologin=true&q=' + query, {
@@ -296,5 +324,6 @@ module.exports = {
   luminai,
   pinterest,
   brat,
+  tiktokdl,
   aiodl
 }
