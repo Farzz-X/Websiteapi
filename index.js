6fe70ae026bc7251bd9d8fee59fde3ff325c8deb
FarisDev <akunkoshonganz@gmail.com>
2025-01-05 14:29:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -162,6 +162,27 @@ app.get('/pindl', async (req, res) => {
   }
 });
 
+app.get('/tiktokdl', async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.json(msg.paramurl);
+
+  try {
+    const data = await tiktokdl(url);
+    if (!data) {
+      return res.json({ status: false, message: msg.nodata });
+    }
+
+    res.json({
+      status: true,
+      creator: author,
+      result: data
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 
 app.get('/instagram', async (req, res) => {
   const url = req.query.url;
