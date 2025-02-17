31f814c1d3ae5db393fefcde01ea4d629119b9f7
Farìsoffc <162881496+Farisofc@users.noreply.github.com>
2025-02-17 10:43:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./message');
-const { tiktok, capcut, igdl, fbdl, aiodl, tiktoksearch, pindl, tiktokDl, listmember, luminai, pinterest, brat, tiktokdll, jadwalSholat } = require('./danz');
+const { tiktok, capcut, igdl, fbdl, aiodl, tiktoksearch, pindl, listmember, luminai, pinterest, brat, tiktokdll, jadwalSholat } = require('./danz');
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const compression = require('compression');
