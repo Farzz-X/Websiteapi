03d24edc1eaaa46f24202789ced7d669178a1a76
Farìsoffc <162881496+Farisofc@users.noreply.github.com>
2025-03-10 02:42:03 +0700

    Update danz.js

--- danz.js
+++ danz.js
@@ -69,7 +69,36 @@ async function jadwalSholat() {
     }
 }
 
+async function islamai(question) {
+    const url = 'https://vercel-server-psi-ten.vercel.app/chat';
+    const data = {
+        text: question,
+        array: [
+            {
+                content: "Assalamualaikum",
+                role: "user"
+            },
+            {
+                content: "Waalaikumsalam",
+                role: "AiSisten"
+            }
+        ]
+    };
 
+    try {
+        const response = await axios.post(url, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
+                'Referer': 'https://islamandai.com/'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error sending request:', error);
+        throw error;
+    }
+}
 
 async function JadwalTvBola() {
   try {
