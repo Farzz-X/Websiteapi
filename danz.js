b6fceb8664eb662535b1455f2ab62879713b237b
Farìsoffc <162881496+Farisofc@users.noreply.github.com>
2025-03-10 16:47:27 +0700

    Update danz.js

--- danz.js
+++ danz.js
@@ -69,6 +69,22 @@ async function jadwalSholat() {
     }
 }
 
+
+async function SimSimi(text, language = 'id') {
+  const { data } = await axios.post("https://api.simsimi.vn/v1/simtalk", new URLSearchParams({
+    text,
+    lc: language
+  }).toString(), {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+  })
+
+  return data.message
+}
+
+
 async function githubStalk(user) {
     return new Promise((resolve, reject) => {
         axios.get(`https://api.github.com/users/${user}`)
